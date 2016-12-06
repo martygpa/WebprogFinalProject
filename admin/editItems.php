f11c1e069b6f167df7e1ddee3326a465ebaa3d4a
ian <if6922@ship.edu>
2016-12-06 10:36:40 -0500

    a

---
+++ admin/editItems.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 11/29/16
+ * Time: 9:30 AM
+ */
+
+/*Add Item*/
+/*Edit Item*/
+/*Delete Item*/
+/*Display Items*/
+
+?>
---
+++ admin/editUsers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 11/29/16
+ * Time: 9:31 AM
+ */
+
+/*Edit User*/
+/*Delete User*/
+/*Display Non-Sensitive User Data*/
+
+?>
