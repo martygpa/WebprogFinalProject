f7a27760f5ed76e2f687a4afc1162c06bcfc9412
Alec Waddelow <alec.waddelow@gmail.com>
2016-12-14 20:03:01 -0500

    More link changes in nav bar

--- Item.php
+++ Item.php
@@ -68,10 +68,10 @@ li a:hover:not(.active) {background-color: #111;}
     <li> <a href="./account_management/Login.html">Login</a></li>
     <li> <a href="/account_management/myAccount.php">My Account</a> </li>
     <li> <a href="/account_management/registerAccount.php">Create New Account</a></li>
-    <li> <a href="/shopping/shoppingCart.php">Shopping Cart</a></li>
-    <li> <a href="/shopping/wishList.php">Wish List</a></li>
-    <li> <a href="/shopping/orderHistory.php">Order History</a></li>
-    <li> <a href="/shopping/checkOut.php">Check Out</a></li>
+    <li> <a href="/ShippingCart/ShoppingCart.php">Shopping Cart</a></li>
+    <li> <a href="/WishList/WishList.php">Wish List</a></li>
+    <li> <a href="/OrderHistory/OrderHistory.php">Order History</a></li>
+    <li> <a href="/Checkout/Checkout.php">Check Out</a></li>
     <?php  if(($_SESSION['isLoggedIn'] == true))
     {
       echo "<li> <a href='../account_management/logout.php'>Logout</a></li>";
